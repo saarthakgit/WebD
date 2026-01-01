5db462724e99a51d6dc29356310af18c2329879f
saarthakgit <saarthakgid@gmail.com>
2026-01-01 23:58:41 +0530

    promises

---
+++ JS/01_dom.js
@@ -0,0 +1,8 @@
+// select parent element and find out values in it
+let parentElement;
+
+parentElement = document.querySelector('.parent')
+console.log(parentElement);  //gives the element
+
+console.log(parentElement.children);
+
---
+++ JS/eventLoops.js
@@ -0,0 +1,11 @@
+// async code
+
+
+// setTimeout(me,2000);
+// function me(){
+//     console.log('sarthakIsomer')
+// }
+
+let mypromise = new Promise( (resolve , reject)=>{
+    
+} )
